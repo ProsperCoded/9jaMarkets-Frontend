1e8995f70618ee799df7067928c3eceabc970c59
youngestage <youngleslie.ik@gmail.com>
2024-09-17 19:00:23 +0100

    deleted learn more button

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -11,9 +11,6 @@ function Hero() {
         </div>
         <h1 className="text-4xl md:text-6xl font-bold mb-4">Discover and Connect with <br /> Nigeria’s Major Markets.</h1>
         <p className="text-xl mb-8">Your gateway to the best products from Nigeria’s leading markets.</p>
-        <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-3 px-6 rounded-full transition duration-300 ease-in-out">
-          Learn More
-        </button>
       </div>
     </div>
   );
