4488f2ad51bc1c6077c6055feb7f9c94ed79c546
osubor-david <osubordavid7@gmail.com>
2024-10-17 02:50:02 -0700

    new ad

--- Frontend/src/components/Adverts.jsx
+++ Frontend/src/components/Adverts.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import  { useState, useRef, useEffect } from 'react';
 import profile from '../assets/profileAD.svg'
 import fashion from '../assets/ad/game-icons_clothes.svg'    
@@ -233,59 +232,58 @@ const [selectedLocation, setSelectedLocation] = useState('Location');
   )
 }
 
-export default Adverts
-=======
-import React from 'react';
+export default Adverts;
+// import React from 'react';
 
-const Advert = ({ subpage }) => {
-  let content;
+// const Advert = ({ subpage }) => {
+//   let content;
 
-  switch (subpage) {
-    case 'my-adverts':
-      content = <MyAdverts />;
-      break;
-    case 'feedback':
-      content = <Feedback />;
-      break;
-    case 'insights':
-      content = <Insights />;
-      break;
-    default:
-      content = <div>Select an option from the sidebar</div>;
-  }
+//   switch (subpage) {
+//     case 'my-adverts':
+//       content = <MyAdverts />;
+//       break;
+//     case 'feedback':
+//       content = <Feedback />;
+//       break;
+//     case 'insights':
+//       content = <Insights />;
+//       break;
+//     default:
+//       content = <div>Select an option from the sidebar</div>;
+//   }
 
-  return (
-    <div>
-      {/* Sidebar and main profile content */}
-      <div className="min-h-screen flex">
-        <aside className="w-1/5 bg-white p-4">
-          <nav>
-            <ul>
-              <li className="py-2 hover:bg-gray-200 rounded-lg">
-                <a href="/profile/my-adverts" className="block text-gray-700">My Adverts</a>
-              </li>
-              <li className="py-2 hover:bg-gray-200 rounded-lg">
-                <a href="/profile/feedback" className="block text-gray-700">Feedback</a>
-              </li>
-              <li className="py-2 hover:bg-gray-200 rounded-lg">
-                <a href="/profile/insights" className="block text-gray-700">Insights</a>
-              </li>
-            </ul>
-          </nav>
-        </aside>
+//   return (
+//     <div>
+//       {/* Sidebar and main profile content */}
+//       <div className="min-h-screen flex">
+//         <aside className="w-1/5 bg-white p-4">
+//           <nav>
+//             <ul>
+//               <li className="py-2 hover:bg-gray-200 rounded-lg">
+//                 <a href="/profile/my-adverts" className="block text-gray-700">My Adverts</a>
+//               </li>
+//               <li className="py-2 hover:bg-gray-200 rounded-lg">
+//                 <a href="/profile/feedback" className="block text-gray-700">Feedback</a>
+//               </li>
+//               <li className="py-2 hover:bg-gray-200 rounded-lg">
+//                 <a href="/profile/insights" className="block text-gray-700">Insights</a>
+//               </li>
+//             </ul>
+//           </nav>
+//         </aside>
 
-        {/* Main profile content */}
-        <main className="w-full p-6 bg-gray-100">
-          {content}
-        </main>
-      </div>
-    </div>
-  );
-};
+//         {/* Main profile content */}
+//         <main className="w-full p-6 bg-gray-100">
+//           {content}
+//         </main>
+//       </div>
+//     </div>
+//   );
+// };
 
-const MyAdverts = () => <div>My Adverts Content</div>;
-const Feedback = () => <div>Feedback Content</div>;
-const Insights = () => <div>Insights Content</div>;
+// const MyAdverts = () => <div>My Adverts Content</div>;
+// const Feedback = () => <div>Feedback Content</div>;
+// const Insights = () => <div>Insights Content</div>;
 
-export default Advert;
->>>>>>> 4af4441a14b7d2867deca64a57e3c79e28e84613
+// export default Advert;
+// >>>>>>> 4af4441a14b7d2867deca64a57e3c79e28e84613
