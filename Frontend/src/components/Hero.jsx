989fb14468f05b3fbd3b977de8bec791edca9889
Psybah <abiodunoluwamurewa@gmail.com>
2024-11-25 16:38:04 +0100

    header and herosection responsiveness complete

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -8,26 +8,23 @@ function Hero() {
       className="relative bg-cover bg-center h-screen"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className="container mx-auto flex flex-col
-                      justify-start items-center h-full text-center
-                      text-white px-4 md:px-10 lg:px-20 pt-16 md:pt-20 lg:pt-24">
+      <div className="container mx-auto flex flex-col justify-center items-center h-full text-center text-white px-4 md:px-10 lg:px-20">
         {/* Logo */}
         <div className="logo mb-8">
           <img
             src={logo}
             alt="9ja Markets"
-            className="h-24 sm:h-36 md:h-48 lg:h-60"
+            className="h-24 sm:h-36 md:h-48 lg:h-56 mx-auto"
           />
         </div>
 
         {/* Heading */}
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 
-        leading-tight">
+        <h1 className="text-2xl sm:text-3xl md:text-5xl lg:text-6xl font-bold mb-4 leading-tight">
           Discover and Connect with <br /> Nigeria’s Major Markets.
         </h1>
 
-        {/* Positioned Paragraph
-        <p className="w-full sm:text-lg md:text-xl py-2 text-center ">
+        {/* Positioned Paragraph (optional) */}
+        {/* <p className="w-full sm:text-lg md:text-xl py-2 text-center ">
           Your gateway to the best products from Nigeria’s leading markets.
         </p> */}
       </div>
