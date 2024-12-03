ab2eaa94ac499d9d672a9b1a05ce28f8202ba933
prosper <enweremproper@gmail.com>
2024-12-03 16:35:28 +0100

    optimize search functionality with useMemo on Home page

--- Frontend/src/components/Malls.jsx
+++ Frontend/src/components/Malls.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import searchIcon from "../assets/search.svg"; // Assuming this is your search icon
 import { Link } from "react-router-dom";
-
+import { MALLS } from "../config";
 const states = [
   "Abuja",
   "Abia",
@@ -43,29 +43,11 @@ const states = [
   "Zamfara",
 ];
 
-const malls = {
-  Abuja: [
-    { name: "Agbalata mall Badagry", img: "/path/to/agbalata.jpg" },
-    {
-      name: "Alaba International mall",
-      img: "/path/to/alaba-international.jpg",
-    },
-    { name: "Ajah mall", img: "/path/to/ajah-mall.jpg" },
-    { name: "Aratumi mall", img: "/path/to/aratumi-mall.jpg" },
-    { name: "Balogun mall, Lagos Island", img: "/path/to/balogun.jpg" },
-    { name: "Bar Beach mall", img: "/path/to/bar-beach-mall.jpg" },
-    { name: "Computer Village", img: "/path/to/computer-village.jpg" },
-    { name: "Èbúté Èrò mall, Lagos Island", img: "/path/to/ebutero-mall.jpg" },
-    { name: "Epe Fish mall", img: "/path/to/epe-fish-mall.jpg" },
-    { name: "Iyana-Iba mall", img: "/path/to/iyana-iba-mall.jpg" },
-  ],
-};
-
 const mallPage = () => {
   const [selectedState, setSelectedState] = useState("Abuja");
   const [searchTerm, setSearchTerm] = useState("");
 
-  const filteredmalls = malls[selectedState]?.filter((mall) =>
+  const filteredmalls = MALLS[selectedState]?.filter((mall) =>
     mall.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
