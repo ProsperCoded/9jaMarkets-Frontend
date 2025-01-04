8beffe9f24f1688a1484406a3b5f9431a88ba088
ProsperCoded <enweremproper@gmail.com>
2025-01-04 05:33:10 +0100

    Add .gitignore and update Profile component for improved navigation and user experience

--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Trash, ChevronsUpDown, Store } from "lucide-react";
+import { Trash, ChevronsUpDown, Store, UserRound } from "lucide-react";
 import { Button } from "./ui/button";
 import {
   Collapsible,
@@ -80,7 +80,7 @@ const Profile = ({ subpage }) => {
         <UserOptions />
         {/* <Link to="/merchant-signup">Merchant Options</Link> */}
         {/* Navigation Links */}
-        <nav>
+        <div>
           <ul className="space-y-4">
             <li>
               <Link
@@ -102,87 +102,51 @@ const Profile = ({ subpage }) => {
                 <span>Products</span>
               </Link>
             </li>
-            <li>
-              <Link
-                to="/orders"
-                className="flex items-center space-x-3 hover:bg-Primary p-2 rounded-lg hover:text-white"
-                style={subpage === "orders" ? styleSelected : {}}
-              >
-                <FontAwesomeIcon icon={faCartShopping} />
-                <span>Orders</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/customers"
-                className="flex items-center space-x-3 hover:bg-Primary p-2 rounded-lg hover:text-white"
-                style={subpage === "customers" ? styleSelected : {}}
-              >
-                <FontAwesomeIcon icon={faUsers} />
-                <span>Customers</span>
-              </Link>
-            </li>
+            {userProfile.userType === "merchant" && (
+              // Customers that contact the merchant
+              <li>
+                <Link
+                  to="/customers"
+                  className="flex items-center space-x-3 hover:bg-Primary p-2 rounded-lg hover:text-white"
+                  style={subpage === "customers" ? styleSelected : {}}
+                >
+                  <FontAwesomeIcon icon={faUsers} />
+                  <span>Contacts</span>
+                </Link>
+              </li>
+            )}
             <li>
               <Link
                 to="/messages"
                 className="flex items-center space-x-3 hover:bg-Primary p-2 rounded-lg hover:text-white"
                 style={subpage === "messages" ? styleSelected : {}}
               >
+                {/* Vendors you have contacted in the past */}
                 <FontAwesomeIcon icon={faMessage} />
-                <span>Messages</span>
+                <span>Vendors</span>
               </Link>
-            </li>
-            <li>
-              <Link
-                to="/wallet"
-                className="flex items-center space-x-3 hover:bg-Primary p-2 rounded-lg hover:text-white"
-                style={subpage === "wallet" ? styleSelected : {}}
-              >
-                <FontAwesomeIcon icon={faWallet} />
-                <span>Wallet</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/premium"
-                className="flex items-center space-x-3 hover:bg-Primary p-2 rounded-lg hover:text-white"
-                style={subpage === "premium" ? styleSelected : {}}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  class="size-6"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-                    clipRuale="evenodd"
-                  />
-                </svg>
-
-                <span>Premium</span>
-              </Link>
-            </li>
-            <li className="pt-10">
-              <div
-                onClick={() => {
-                  setLogoutOpen(true);
-                }}
-                className="flex items-center space-x-3 hover:bg-red-100 p-2 pt-25 rounded-lg text-red-500 hover:text-red-700 cursor-pointer"
-              >
-                <FontAwesomeIcon icon={faSignOutAlt} />
-                <span>Logout</span>
-              </div>
-              <div>
-                <h3 className="flex items-center space-x-3 hover:bg-red-100 p-2 pt-25 rounded-lg text-red-500 hover:text-red-700 cursor-pointer">
-                  <Trash className="mr-1" size={20} />
-                  Delete Account
-                </h3>
-              </div>
             </li>
           </ul>
-        </nav>
+        </div>
+        <ul className="mt-auto">
+          <li className="pt-10">
+            <div
+              onClick={() => {
+                setLogoutOpen(true);
+              }}
+              className="flex items-center space-x-3 hover:bg-red-100 p-2 pt-25 rounded-lg text-red-500 hover:text-red-700 cursor-pointer"
+            >
+              <FontAwesomeIcon icon={faSignOutAlt} />
+              <span>Logout</span>
+            </div>
+            <div>
+              <h3 className="flex items-center space-x-3 hover:bg-red-100 p-2 pt-25 rounded-lg text-red-500 hover:text-red-700 cursor-pointer">
+                <Trash className="mr-1" size={20} />
+                Delete Account
+              </h3>
+            </div>
+          </li>
+        </ul>
       </aside>
 
       {/* Main Content */}
@@ -262,7 +226,7 @@ function UserOptions() {
             }}
           >
             <div className="flex items-center gap-2">
-              <Store />
+              <UserRound />
               <h4 className="font-semibold">Customer Options</h4>
             </div>
             <CollapsibleTrigger asChild>
