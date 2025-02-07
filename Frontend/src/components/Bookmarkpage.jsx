df97f46dc39ebfa02c9c19fec68bac389d1838b8
ProsperCoded <enweremproper@gmail.com>
2025-02-07 12:12:17 +0100

    Add Interswitch payment integration apis and support

--- Frontend/src/components/Bookmarkpage.jsx
+++ Frontend/src/components/Bookmarkpage.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const BookmarksPage = () => {
-  const [bookmarks, setBookmarks] = useState([]);
+  const [bookmarks, setBookmarks] = useState([{}]);
 
   useEffect(() => {
     // Fetch bookmarks from local storage or API
@@ -19,23 +19,29 @@ const BookmarksPage = () => {
   };
 
   return (
-    <div className="w-full max-w-md h-[100vh] container mx-auto p-4">
+    <div className="mx-auto p-4 w-full max-w-md h-[100vh] container">
       <div className="flex items-center mb-4">
         <Link to="/" className="text-Primary hover:text-orange">
           <FontAwesomeIcon icon={faArrowLeft} className="w-6 h-6" />
         </Link>
-        <h1 className="text-2xl font-bold ml-4 text-Primary ">Bookmarks</h1>
+        <h1 className="ml-4 font-bold text-2xl text-Primary">Bookmarks</h1>
       </div>
       {bookmarks.length === 0 ? (
-        <p className="text-Primary text-center">No bookmarks yet.</p>
+        <p className="text-center text-Primary">No bookmarks yet.</p>
       ) : (
         <ul className="space-y-4">
           {bookmarks.map((item) => (
-            <li key={item.id} className="flex justify-between items-center p-4 bg-gray-800 rounded-md">
+            <li
+              key={item.id}
+              className="flex justify-between items-center bg-gray-800 p-4 rounded-md"
+            >
               <Link to={item.link} className="text-orange hover:underline">
                 {item.title}
               </Link>
-              <button onClick={() => removeBookmark(item.id)} className="text-red-500 hover:text-red-700">
+              <button
+                onClick={() => removeBookmark(item.id)}
+                className="text-red-500 hover:text-red-700"
+              >
                 <FontAwesomeIcon icon={faTrash} className="w-5 h-5" />
               </button>
             </li>
