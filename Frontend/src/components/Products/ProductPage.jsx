62113bb535350d7da6a0ae85cf20801496ba3810
ProsperCoded <enweremproper@gmail.com>
2025-02-12 11:16:24 +0100

    feat: add Sheet component and fixed some responsiveness issues

--- Frontend/src/components/Products/ProductPage.jsx
+++ Frontend/src/components/Products/ProductPage.jsx
@@ -2,19 +2,21 @@
 
 import * as React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
 import AddProduct from "./AddProduct";
 import ViewProducts from "./ViewProduct";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+
 export default function ProductPage() {
   const navigate = useNavigate();
   const location = useLocation();
+
   if (location.pathname !== "/dashboard/products") {
     return <Outlet />;
   }
+
   return (
-    <div className="mx-auto p-4 w-full max-w-6xl">
+    <div className="mx-auto w-full max-w-6xl">
       <Tabs defaultValue="add" className="w-full">
         <TabsList className="grid grid-cols-2 mb-8 w-full">
           <TabsTrigger
@@ -25,10 +27,10 @@ export default function ProductPage() {
           </TabsTrigger>
           <TabsTrigger value="view">View Products</TabsTrigger>
         </TabsList>
-        <TabsContent value="add">
+        <TabsContent value="add" className="p-4">
           <AddProduct />
         </TabsContent>
-        <TabsContent value="view">
+        <TabsContent value="view" className="p-4">
           <ViewProducts />
         </TabsContent>
       </Tabs>
