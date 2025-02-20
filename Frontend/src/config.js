41b569bf9c2d7b14a2daaf7b193930d5ae9b8264
ProsperCoded <enweremproper@gmail.com>
2025-02-20 15:16:19 +0100

    Update product categories in config; simplify and align with server support

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -7,22 +7,34 @@ export const ACCESS_TOKEN_DURATION = 3;
 // 14 Days for refresh token
 export const REFRESH_TOKEN_DURATION = 14;
 
+// ? This is what the server current supports
 export const PRODUCT_CATEGORIES = [
-  "EDUCATION & STATIONERY",
-  "REAL ESTATE & HOUSING",
-  "EVENTS & ENTERTAINMENT",
-  "TECHNOLOGY SERVICES",
-  "CULTURAL EXPERIENCES",
-  "FOOD & GROCERIES",
   "ELECTRONICS",
-  "FASHION & ACCESSORIES",
-  "HEALTH & WELLNESS",
-  "HOME & LIVING",
-  "AUTOMOBILE NEEDS",
-  "TRADITIONAL CRAFTS",
-  "SPORTS & OUTDOOR",
-  "KIDS & BABY PRODUCTS",
+  "FASHION",
+  "FOOD",
+  "HEALTH",
+  "HOME",
+  "SPORTS",
+  "TOYS",
 ];
+
+// ! The server doesn't support this yet
+// export const PRODUCT_CATEGORIES = [
+//   "EDUCATION & STATIONERY",
+//   "REAL ESTATE & HOUSING",
+//   "EVENTS & ENTERTAINMENT",
+//   "TECHNOLOGY SERVICES",
+//   "CULTURAL EXPERIENCES",
+//   "FOOD & GROCERIES",
+//   "ELECTRONICS & GADGETS",
+//   "FASHION & ACCESSORIES",
+//   "HEALTH & WELLNESS",
+//   "HOME & LIVING",
+//   "AUTOMOBILE NEEDS",
+//   "TRADITIONAL CRAFTS",
+//   "SPORTS & OUTDOOR",
+//   "KIDS & BABY PRODUCTS",
+// ];
 export const plans = {
   standard: {
     id: "standard",
