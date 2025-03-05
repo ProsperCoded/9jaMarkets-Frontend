8d4f317f9d22baadd5fdcfbf0811f1c0ca069ba7
Psybah <abiodunoluwamurewa@gmail.com>
2025-03-05 16:23:39 +0100

    Added osun state images

--- Frontend/scripts/Malls.cjs
+++ Frontend/scripts/Malls.cjs
@@ -218,268 +218,114 @@ const MALLS = {
   Oyo: [
     {
       name: "The Palms Shopping Mall",
-      img: "./markets/ThePalmsShoppingMall.jpg",
+      img: "./malls/oyo/ThePalmsShoppingMall.png",
       address: "MKO Abiola Way, Oluyole 200273, Ibadan",
       city: "Ibadan",
       description: "A spacious mall offering a variety of retail outlets, eateries, and entertainment options."
     },
     {
       name: "Heritage Mall",
-      img: "./markets/HeritageMall.jpg",
+      img: "./malls/oyo/HeritageMall.jpg",
       address: "Dugbe, Ibadan",
       city: "Ibadan",
       description: "A modern shopping center located in the heart of Ibadan, featuring numerous shops and services."
     },
     {
       name: "Ventura Mall",
-      img: "./markets/VenturaMall.jpg",
+      img: "./malls/oyo/VenturaMall.jpg",
       address: "Samonda, Ibadan",
       city: "Ibadan",
       description: "A vibrant mall offering shopping, dining, and entertainment experiences, including a cinema."
     },
     {
       name: "Jericho Mall",
-      img: "./markets/JerichoMall.jpg",
+      img: "./malls/oyo/JerichoMall.jpg",
       address: "Jericho, Ibadan",
       city: "Ibadan",
       description: "A mid-size mall providing prime retail shops and attractions aimed at creating a memorable shopping experience."
     },
     {
       name: "Grandex Mall",
-      img: "./markets/GrandexMall.jpg",
+      img: "./malls/oyo/GrandexMall.jpg",
       address: "Bodija, Ibadan",
       city: "Ibadan",
       description: "A shopping destination offering a variety of products, including groceries, electronics, and fashion items."
     },
     {
       name: "Ace Mall",
-      img: "./markets/AceMall.jpg",
+      img: "./malls/oyo/AceMall.jpg",
       address: "Ring Road, Ibadan",
       city: "Ibadan",
       description: "A contemporary mall featuring various retail outlets, eateries, and entertainment options."
     },
     {
-      name: "Grand Mall",
-      img: "./markets/GrandMall.jpg",
-      address: "Challenge, Ibadan",
-      city: "Ibadan",
-      description: "A shopping center offering a range of retail stores, services, and dining options."
-    },
-    {
       name: "Westmead Mall",
-      img: "./markets/WestmeadMall.jpg",
+      img: "./malls/oyo/WestmeadMall.jpg",
       address: "Rayfield Gardens City Estate, Ibadan",
       city: "Ibadan",
       description: "A newly opened shopping center set to revolutionize the shopping experience in Ibadan."
     },
     {
       name: "Brent Mall Sawmill Branch",
-      img: "./markets/BrentMallSawmillBranch.jpg",
+      img: "./malls/oyo/BrentMallSawmillBranch.jpg",
       address: "Old Sawmill Bus Stop, Old Ife Road, Ibadan",
       city: "Ibadan",
       description: "A retail outlet dealing in agricultural products, healthcare products, and pharmaceuticals."
-    },
-    {
-      name: "Topfat Art Gallery",
-      img: "./markets/TopfatArtGallery.jpg",
-      address: "Ibadan",
-      city: "Ibadan",
-      description: "An art gallery offering a variety of artworks and cultural artifacts."
-    },
-    {
-      name: "Constance And Sons Gallery",
-      img: "./markets/ConstanceAndSonsGallery.jpg",
-      address: "Ibadan",
-      city: "Ibadan",
-      description: "A gallery showcasing diverse artworks and cultural items."
-    },
-    {
-      name: "Richmary Discovery Ltd",
-      img: "./markets/RichmaryDiscoveryLtd.jpg",
-      address: "Shop 29, Praise Plazer, Iwo Road, Ibadan",
-      city: "Ibadan",
-      description: "A store offering a range of products, including computers and accessories."
-    },
-    {
-      name: "SAMTOB Phones & Computers",
-      img: "./markets/SAMTOBPhonesAndComputers.jpg",
-      address: "T24, GSM Plaza, Mokola Roundabout, Ibadan",
-      city: "Ibadan",
-      description: "A store specializing in quality laptops, mobile phones, and accessories."
-    },
-    {
-      name: "Compustore",
-      img: "./markets/Compustore.jpg",
-      address: "13 Adeniji Street, Liberty Rd, Ibadan",
-      city: "Ibadan",
-      description: "An online store dealing in the sales of computers and related accessories."
-    },
-    {
-      name: "First Admin Computer Technology",
-      img: "./markets/FirstAdminComputerTechnology.jpg",
-      address: "79 beside Henry Tee Motor, Ring Road, Ibadan",
-      city: "Ibadan",
-      description: "A company providing advanced computer technology solutions and services."
     }
   ],
 
   Ekiti: [
     {
       name: "Staleg Mall",
-      img: "./markets/StalegMall.jpg",
+      img: "./malls/ekiti/StalegMall.jpg",
       address: "Ori-Apata, Adebayo Road",
       city: "Ado-Ekiti",
       description: "Offers luxury wears, quality shoes and bags, cosmetics, electronics, and maternity essentials."
     },
     {
-      name: "Ekiti State Textile Shopping Mall",
-      img: "./markets/EkitiStateTextileShoppingMall.jpg",
-      address: "J6P5+WH2, Ado-Ekiti 360102",
-      city: "Ado-Ekiti",
-      description: "A textile shopping center located in Ado-Ekiti."
-    },
-    {
       name: "Dominion Mall",
-      img: "./markets/DominionMall.jpg",
+      img: "./malls/ekiti/DominionMall.png",
       address: "Opposite Ijigbo Roundabout",
       city: "Ado-Ekiti",
       description: "Offers electronics, furniture, and beddings; known for quality and affordability."
     },
     {
       name: "Arena Shopping Mall",
-      img: "./markets/ArenaShoppingMall.jpg",
+      img: "./malls/ekiti/ArenaShoppingMall.jpg",
       address: "Okeila Road",
       city: "Ado-Ekiti",
       description: "A marketplace where people of different tribes come together to buy and sell; known for affordable vegetables and fruits."
     },
     {
-      name: "Folorunsho Memorial Shopping Complex",
-      img: "./markets/FolorunshoMemorialShoppingComplex.jpg",
-      address: "Nova Road",
-      city: "Ado-Ekiti",
-      description: "Accessible shopping complex known for its variety of stores."
-    },
-    {
-      name: "ABUAD Shopping Mall",
-      img: "./markets/ABUADShoppingMall.jpg",
-      address: "ABUAD",
-      city: "Ado-Ekiti",
-      description: "Fully stocked mall offering electronics, groceries, boutiques, salons, and more."
-    },
-    {
-      name: "Oyinbo Shopping & Retail",
-      img: "./markets/OyinboShoppingAndRetail.jpg",
-      address: "Plot Q6 behind Ajaleye Civic Centre, Fed Poly Road",
-      city: "Ado-Ekiti",
-      description: "Offers a wide variety of items from foodstuff to housewares."
-    },
-    {
       name: "Kaita Plaza",
-      img: "./markets/KaitaPlaza.jpg",
+      img: "./malls/ekiti/KaitaPlaza.jpeg",
       address: "Off Ado-Iworoko-Ifaki Road",
       city: "Ado-Ekiti",
       description: "A large shopping center comprising offices, stalls, and shops selling different kinds of items."
-    },
-    {
-      name: "Fayose Market",
-      img: "./markets/FayoseMarket.jpg",
-      address: "Ajilosun Street, Ado-Ikere Road",
-      city: "Ado-Ekiti",
-      description: "Known as the Computer Village of Ekiti, specializing in ICT accessories, mobile phones, gadgets, and electronics."
-    },
-    {
-      name: "Eyecatch Accessories",
-      img: "./markets/EyecatchAccessories.jpg",
-      address: "7, Ekute Quarters",
-      city: "Ado-Ekiti",
-      description: "Leading eyeglasses distribution company offering various types of glasses."
-    },
-    {
-      name: "Funmola House of Fashion",
-      img: "./markets/FunmolaHouseOfFashion.jpg",
-      address: "Bolorunduro Street",
-      city: "Ado-Ekiti",
-      description: "Specializes in all forms of female clothing and turban caps."
-    },
-    {
-      name: "Goldenspa Shopping Mall",
-      img: "./markets/GoldenspaShoppingMall.jpg",
-      address: "Nova Road after CAC, Basiri",
-      city: "Ado-Ekiti",
-      description: "Offers products such as wine, provisions, and more at discounted prices."
-    },
-    {
-      name: "Lasting Impressions",
-      img: "./markets/LastingImpressions.jpg",
-      address: "No 143, Ajilosun Street, opposite St. Michael's Primary School",
-      city: "Ado-Ekiti",
-      description: "Offers sales of gift items, flowers, and accessories."
-    },
-    {
-      name: "Modupe Complex",
-      img: "./markets/ModupeComplex.jpg",
-      address: "Bisi Egbeyemi Crescent, Onigaari Road, GRA",
-      city: "Ado-Ekiti",
-      description: "A shopping store offering high-quality, custom-made fabric materials and textiles."
-    },
-    {
-      name: "Vicano Boutique & Suits",
-      img: "./markets/VicanoBoutiqueAndSuits.jpg",
-      address: "Ijigbo Street, Irona Quarters Road",
-      city: "Ado-Ekiti",
-      description: "An in-store shopping destination for a wide range of men's wear, including shoes, suits, and casual wear."
     }
   ],
 
   Osun: [
     {
       name: "Osun Mall",
-      img: "./markets/OsunMall.jpg",
+      img: "./malls/osun/OsunMall.jpg",
       address: "Gbongan/Ibadan Road, Osogbo",
       city: "Osogbo",
       description: "An ultramodern mega shopping mall featuring Filmhouse cinemas, spas, game shops, bars, restaurants, and various retail stores."
     },
     {
       name: "Betsay Shopping Mall",
-      img: "./markets/BetsayShoppingMall.jpg",
+      img: "./malls/osun/BetsayShoppingMall.jpg",
       address: "Obafemi Awolowo Way, Osogbo",
       city: "Osogbo",
       description: "A shopping destination offering a variety of jewelry and other retail stores."
     },
     {
       name: "Bridget's Mall",
-      img: "./markets/BridgetsMall.jpg",
+      img: "./malls/osun/BridgetsMall.png",
       address: "Ile-Ife",
       city: "Ile-Ife",
       description: "A privately owned 'one-stop' shopping and social hub offering a range of goods and services."
-    },
-    {
-      name: "Olorunda Shopping Complex",
-      img: "./markets/OlorundaShoppingComplex.jpg",
-      address: "Okefia, Osogbo",
-      city: "Osogbo",
-      description: "A shopping complex offering various retail outlets and services."
-    },
-    {
-      name: "Ayeso Shopping Complex",
-      img: "./markets/AyesoShoppingComplex.jpg",
-      address: "Ayeso Area, Esa-Oke",
-      city: "Esa-Oke",
-      description: "A local shopping complex providing various goods and services to the community."
-    },
-    {
-      name: "Fasogbon Shopping Complex",
-      img: "./markets/FasogbonShoppingComplex.jpg",
-      address: "Edunabon Road, Ile-Ife",
-      city: "Ile-Ife",
-      description: "A shopping complex offering a variety of retail stores and services."
-    },
-    {
-      name: "Segron Mart",
-      img: "./markets/SegronMart.jpg",
-      address: "Plot 11A Oodua Street, Unity Estate, Osogbo",
-      city: "Osogbo",
-      description: "An in-store retail shopping center offering a wide variety of grocery items, provisions, and daily consumables."
     }
   ],
 
